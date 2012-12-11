0200e1e79ddb84e9c30aef438bfa0b0bae5e038c
Lance Sanchez <lance_sanchez@rapid7.com>
2012-12-10 18:10:58 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LunchQuizzer::Application.config.secret_token = '035d5cbe1e3c5b9436a5c847e05c8b82af008f9d614142e354187481e4fcf2d4b28d45ec8e31679faec8942d6bc4e643e6f4dee4ff3be9a97b19540a2ea1afbc'
